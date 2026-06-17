854913f5b3fb30f8e6dc4a1e4582934836eb4f2e
kayquelopes <kaiquelopes.601@gmail.com>
2026-06-16 21:41:55 -0300

    feat: add comment for local development API URL replacement in apiConfig.js

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,2 +1,4 @@
 // src/apiConfig.js
 export const API_URL = "https://api-impressoes-kayque-99.onrender.com";
+
+// REPLACE DE https://api-impressoes-kayque-99.onrender.com PARA http://localhost:8080 DURANTE O DESENVOLVIMENTO LOCAL, SE NECESSÁRIO.
